951e3b52c79da2b9b753d9050e28cfcde2fc156e
Gagarinov-ops <gagarinoff.one@yandex.ru>
2026-04-13 18:22:44 +0700

    сайт-портфолио на Astro. Изменил astro.config добавил site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://imaginative-yeot-668b88.netlify.app', // <- вот сюда
   vite: {
     plugins: [tailwindcss()]
   }
